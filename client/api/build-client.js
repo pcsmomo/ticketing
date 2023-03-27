fb70665859e957dfac1ec7aaca260463dac24a02
Noah Kim <dongwoo_333@hanmail.net>
2023-03-27 19:01:46 +1100

    chore: change the domain name for client(gui)

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,8 +5,7 @@ const buildClient = ({ req }) => {
     // we are on the server
 
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://ticketing-prod.site',
       headers: req.headers,
     });
   } else {
